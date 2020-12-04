47b18da6b10bf96808ed1b884b8c2ceaa602a8dd
Freditansari <Freditansari@users.noreply.github.com>
2020-12-04 07:54:37 -0500

    added template for card

---
+++ src/Components/InventoryCard/InventoryCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const InventoryCard = props => {
+    return (
+        <div>
+            <div className="card">
+                <div className="row">
+                    <div className="column">
+
+                    </div>
+                    <div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+InventoryCard.propTypes = {
+
+}
+
+export default InventoryCard
